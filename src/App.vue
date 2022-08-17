fe538abb49625a869bcb64fbd8ebd58b26759be7
Mizu <psdk55052004@gmail.com>
2022-08-17 16:13:41 +0800

    新增 文字大小切換

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/test">test</router-link>
+    <ul class="d-flex list-unstyled">
+      <li class="mx-3">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="mx-3">
+        <router-link to="/about">About</router-link>
+      </li>
+      <li class="mx-3">
+        <router-link to="/test">test</router-link>
+      </li>
+      <li class="mx-3">
+        字型：
+        <ChangeFontSize></ChangeFontSize>
+      </li>
+    </ul>
+
   </nav>
   <router-view />
 </template>
+
+<script>
+import ChangeFontSize from '@/components/ChangeFontSize.vue'
+export default {
+  components: {
+    ChangeFontSize
+  },
+
+  mounted () {
+  }
+}
+</script>
 
 <style lang="scss">
 @import "@/assets/stylesheets/all.scss";
---
+++ src/components/ChangeFontSize.vue
@@ -0,0 +1,42 @@
+<template>
+  <select @change="changeSize" v-model="currentFontSize">
+    <option :value="font" v-for="(style, font) in fontSize" :key="font">
+      {{ font }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      fontSize: {
+        大: 'font-size: 25px !important;',
+        中: 'font-size: 20px !important;',
+        小: 'font-size: 16px !important;'
+      },
+      currentFontSize: localStorage.getItem('fontSize') || '小'
+    }
+  },
+
+  watch: {
+  },
+
+  methods: {
+    //* 切換文字大小
+    changeSize (e) {
+      const fs = e.target.value
+      localStorage.setItem('fontSize', fs)
+      document.body.style = this.fontSize[fs]
+    }
+  },
+
+  mounted () {
+    document.body.style = this.fontSize[this.currentFontSize]
+  }
+
+}
+</script>
+
+<style lang='scss' scope></style>
